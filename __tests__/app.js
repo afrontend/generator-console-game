c6d1e316617167fbbeae2e74254d82bc137372c0
afrontend <frontend.wordpress@gmail.com>
2019-05-16 22:07:39 +0900

    remove test code of template

--- __tests__/app.js
+++ __tests__/app.js
@@ -6,11 +6,11 @@ const helpers = require("yeoman-test");
 describe("generator-console-game:app", () => {
   beforeAll(() => {
     return helpers
-      .run(path.join(__dirname, "../generators/app"))
+      .run(path.join(__dirname, "../app"))
       .withPrompts({ someAnswer: true });
   });
 
   it("creates files", () => {
-    assert.file(["dummyfile.txt"]);
+    assert.file(["package.json"]);
   });
 });
--- app/templates/lib/__tests__/fpBlock.test.js
+++
@@ -1,8 +0,0 @@
-const assert = require("assert");
-const game = require("../index.js");
-
-describe("fpSpace", () => {
-  it("init return missilePanel", () => {
-    assert(game !== undefined, "should be defined game");
-  });
-});
